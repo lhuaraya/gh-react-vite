e5ac7d5247d839c6022c726fa57f44b6917955ca
Luigi Huaraya <lhuaraya@qualityobjects.com>
2023-04-03 00:48:06 +0200

    sdfs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://gndx.github.io/gh-react-vite',
+  base: 'https://lhuaraya.github.io/gh-react-vite',
   plugins: [react()],
 })
